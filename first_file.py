e1c24d124d0a05b59b5cb30a9ea671433ee3653b
Dmitry Chuveev <dmitry.chuveev@gmail.com>
2018-08-30 17:20:46 -0700

    Создан файл first_file.py
    Создан файл для тестирования списков

---
+++ first_file.py
@@ -0,0 +1,23 @@
+l = [1,2,3]
+
+l.append('5')
+
+a = l.count(2)
+
+x = [1,2,3,4,5,6,7]
+
+x.append([8,9])
+
+x.remove([8,9])
+
+x.extend([8,9])
+
+x.index(1)
+
+x.insert(2,'inserted')
+
+y = x.pop()
+
+x.remove('inserted')
+
+print(x)
